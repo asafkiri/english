eaeea5a138fd29ad7e45c8d8266e3987e634f724
asafkiri <asafkiri@gmail.com>
2026-08-15 18:07:27 +0300

    Build child-friendly English learning map

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'english-adventure-v1';
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request).then(response => {
+        if(response && response.ok){
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => cached || (event.request.mode === 'navigate' ? caches.match('./index.html') : undefined));
+      return cached || network;
+    })
+  );
+});
